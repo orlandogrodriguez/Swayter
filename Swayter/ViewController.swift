d1f84a901707ed2ac355b6f6117e898b7a105c90
Orlando G. Rodriguez <orlandogrodriguez94@gmail.com>
2017-08-05 10:33:11 -0400

    Initial Commit

---
+++ Swayter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swayter
+//
+//  Created by Orlando G. Rodriguez on 8/5/17.
+//  Copyright © 2017 Worly Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
