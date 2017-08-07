680a0691034d6b0d566fbd4fe3690fd979d96a95
Orlando G. Rodriguez <orlandogrodriguez94@gmail.com>
2017-08-07 16:05:09 -0400

    Created new project for Swayter . Integrated with firebase and created base classes.

--- Swayter/New Group/Threshold.swift
+++ Swayter/New Group/Threshold.swift
@@ -7,3 +7,62 @@
 //
 
 import Foundation
+
+class Threshold {
+    /*
+     *  Class Variables
+     */
+    private var name:String
+    private var minValue:Int
+    private var maxValue:Int
+    private var curValue:Int
+    
+    /*
+     *  Initializers
+     */
+    init() {
+        name = "Sweater"
+        minValue = 0
+        maxValue = 100
+        curValue = 70
+    }
+    
+    init(name:String, val:Int) {
+        self.name = name
+        curValue = val
+        minValue = 0
+        maxValue = 100
+    }
+    
+    /*
+     *  Firebase Methods
+     */
+    
+    /*
+     *  Getter Methods Variables
+     */
+    func getName() -> (String) {
+        return name
+    }
+    
+    func getCurValue() -> (Int) {
+        return curValue
+    }
+    
+    /*
+     *  Setter Methods
+     */
+    func setName(newName:String) {
+        name = newName
+    }
+    
+    func setCurValue(newValue:Int) {
+        curValue = newValue
+    }
+    
+    /*
+     *  Responsibility Methods
+     */
+    
+
+}
--- Swayter/New Group/Weatherman.swift
+++ Swayter/New Group/Weatherman.swift
@@ -7,3 +7,77 @@
 //
 
 import Foundation
+
+class Weatherman {
+    
+    
+    var F_UNITS = 0;
+    var C_UNITS = 1;
+    /*
+     *  Class Variables
+     */
+    var currentTemperature: Int?
+    var maxTemperatureForToday:Int?
+    var minTemperatureForToday:Int?
+    var messageOfTheDay:String
+    var units:Int
+    
+    /*
+     *  Initializers
+     */
+    init() {
+        //TODO: acquire weatehr information
+        currentTemperature = 75
+        maxTemperatureForToday = 80
+        minTemperatureForToday = 65
+        messageOfTheDay = "Loading..."
+        units = F_UNITS
+    }
+    
+    /*
+     *  Getter Methods
+     */
+    func getCurrentTemperature() -> Int {
+        return currentTemperature!
+    }
+    
+    func getMaxTemperatureForToday() -> Int {
+        return maxTemperatureForToday!
+    }
+    
+    func getMinTemperatureForToday() -> Int {
+        return minTemperatureForToday!
+    }
+    
+    func getMessageOfTheDay() -> String {
+        return messageOfTheDay
+    }
+    
+    /*
+     *  Setter Methods
+     */
+    func setCurrentTemperature(newCurrentTemperature:Int) {
+        currentTemperature = newCurrentTemperature
+    }
+    
+    func setMaxTemperatureForToday(newMaxTemperature:Int) {
+        maxTemperatureForToday = newMaxTemperature
+    }
+    
+    func setMinTemperatureForToday(newMinTemperature:Int) {
+        minTemperatureForToday = newMinTemperature
+    }
+    
+    func setMessageOfTheDay(newMessageOfTheDay:String) {
+        messageOfTheDay = newMessageOfTheDay
+    }
+    
+    
+    /*
+     *  Responsibility Methods
+     */
+    
+    func toCelcius(temp:Int) -> Int {
+        return Int((5.0 / 9.0) * Double(getCurrentTemperature() - 32))
+    }
+}
