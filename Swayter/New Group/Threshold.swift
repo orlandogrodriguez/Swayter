0c8f76b20852b729d31f35f69301a87e15fe68cd
Orlando G. Rodriguez <orlandogrodriguez94@gmail.com>
2017-08-07 16:04:46 -0400

    Created new project for Swayter . Integrated with firebase and created base classes.

---
+++ Swayter/New Group/Threshold.swift
@@ -0,0 +1,9 @@
+//
+//  Threshold.swift
+//  Swayter
+//
+//  Created by Orlando G. Rodriguez on 8/5/17.
+//  Copyright © 2017 Worly Software. All rights reserved.
+//
+
+import Foundation
---
+++ Swayter/New Group/Weatherman.swift
@@ -0,0 +1,9 @@
+//
+//  Weatherman.swift
+//  Swayter
+//
+//  Created by Orlando G. Rodriguez on 8/5/17.
+//  Copyright © 2017 Worly Software. All rights reserved.
+//
+
+import Foundation
